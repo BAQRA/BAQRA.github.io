0732586d9f1dba8bfc3546b11af29f2f1b1aa7cd
BAQRA <levaniko.b2003@gmail.com>
2021-11-29 16:26:35 +0400

    nodeshop1

--- myburger/src/axios-orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://myburger-r.firebaseio.com/'
-})
-
-
-export default instance;
--- myburger/src/components/UI/Spinner/Spinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Classes from './Spinner.css'
-
-const spinner = () => (
-    <div className={Classes.Loader}>Loading...</div>
-);
-
-export default spinner;
